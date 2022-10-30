32bd9a0b90e769a2bdc98a6fe305bb83f99ecb3f
Ranjeet poojari <ranjeetpoojari77@gmail.com>
2022-10-30 23:31:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+$('.course-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        700: {
+            items: 3,
+        },
+        1000: {
+            items: 6,
+        }
+    }
+})
+$('.faculty-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        700: {
+            items: 2,
+        },
+        1000: {
+            items: 2,
+        }
+    }
+})
+$('.program-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        700: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        }
+    }
+})
+
