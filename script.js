0a92e70705dfa81a94f0180c99b2846b9c452f27
Ranjeet poojari <ranjeetpoojari77@gmail.com>
2022-12-15 10:47:12 +0530

    second commit

--- script.js
+++ script.js
@@ -15,10 +15,22 @@ $('.course-carousel').owlCarousel({
         }
     }
 })
+$('.course-carousel1').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2,
+        },
+       
+    }
+})
 $('.faculty-carousel').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
+    center:true,
     responsive: {
         0: {
             items: 1,
@@ -26,7 +38,10 @@ $('.faculty-carousel').owlCarousel({
         700: {
             items: 2,
         },
-        1000: {
+        902:{
+            items:1
+        },
+        1200: {
             items: 2,
         }
     }
@@ -47,4 +62,35 @@ $('.program-carousel').owlCarousel({
         }
     }
 })
+
+$(document).ready(function(){
+    $.validator.addMethod("Phonenumber",function(value){
+        return /[0-9]{10}/.test(value);
+    },"Number only");
+$("#myform").validate({
+ rules:{
+    u_name:{
+        required:true,
+        minlength:3,
+        lettersonly:true
+    },
+    u_email:{required:true,email:true},
+    u_city:{required:true,lettersonly:true},
+    u_phone:{required:true,minlength:10,Phonenumber:true},
+    u_message:{required:true}
+ },
+ messages:{
+    u_name:{
+        required:"Please Enter Your Name",
+        minlength:"Name should be more then 3 characters",
+    },
+    u_email:{required:"Please Enter Your Email address"},
+    u_city:{required:"Please Enter Your City Name"},
+    u_phone:{
+        required:"Please Enter Your Phone number",
+    minlength:"Phone number Should be 10 charactor"},
+    u_message:{required:"Please give your feedback"}
+ }
+});
+});
 
